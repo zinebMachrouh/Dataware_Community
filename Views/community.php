a597d3262c807b29a03cb0c1d23bf62e70ddbfdd
AmalLemghari <lemghari99amal@gmail.com>
2023-12-07 10:32:44 +0100

    hello

--- Views/community.php
+++ Views/community.php
@@ -16,6 +16,8 @@ include "../SQL/connect.php";
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel="shortcut icon" href="../public/brand.png" type="image/x-icon">
     <link rel="stylesheet" href="../public/style1.css" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
     <script src="https://kit.fontawesome.com/6e1faf1eda.js" crossorigin="anonymous"></script>
     <!-- fonts -->
     <script src="https://cdn.tailwindcss.com"></script>
@@ -24,7 +26,6 @@ include "../SQL/connect.php";
             theme: {
                 fontFamily: {
                     'Saira': ['Saira Condensed', 'sans-serif']
-
                 },
                 extend: {
                     colors: {
@@ -158,14 +159,47 @@ include "../SQL/connect.php";
                         <h3 class="pt-4 text-lg text-font-medium"><?php echo $question['content']; ?></h2>
                     </div>
                     <div class="p-4 flex items-center gap-10 mt-2 text-white">
-                        <a href="" class="flex items-center border border-gray-300 px-2 py-1 rounded-lg">
-                            <svg class="pr-1" xmlns="http://www.w3.org/2000/svg" height="28" width="28" viewBox="0 0 448 512">
+                        <!-- <a href="" class="flex items-center border border-gray-300 px-2 py-1 rounded-lg"> -->
+                        <button data-modal-target="default-modal" data-modal-toggle="default-modal" class="block text-white border border-gray-300 bg-none hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Add Answer +</button>
+                            <!-- <svg class="pr-1" xmlns="http://www.w3.org/2000/svg" height="28" width="28" viewBox="0 0 448 512">
                                 <path fill="#ffffff" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                            </svg>
-                            Add an answer
-                        </a>
+                            </svg> -->
+                            <!-- Add an answer -->
+                        <!-- </a> -->
+                        
                         <p class="border border-gray-300 px-2 rounded-full">HTML</p>
                         <p class="border border-gray-300 px-2 rounded-full">CSS</p>
+
+                        <div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div class="relative p-4 w-full max-w-2xl max-h-full">
+                                <!-- Modal content -->
+                                <div class="relative bg-white rounded-lg shadow dark:bg-white-700">
+                                    <!-- Modal header -->
+                                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                     <!-- <img src="../public/logo.png" alt="Logo" class="h-8 w-auto" /> -->
+                                        <h3 class="text-xl font-semibold text-gray-900 dark:text-black">
+                                            What is the main objective of this project?
+                                        </h3>
+                                        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                            </svg>
+                                            <span class="sr-only">Close modal</span>
+                                        </button>
+                                    </div>
+                                    <div class="p-4 md:p-5 space-y-4">
+                                        <textarea id="modalContent" class="w-full p-2 border rounded-md text-base leading-relaxed text-black dark:text-gray-400 resize-none" rows="5">
+                                        </textarea>
+                                    </div>
+                                    <!-- Modal footer -->
+                                    <div class="flex items-center justify-end p-4 md:p-5 border-t-0 border-gray-200 rounded-b dark:border-gray-600">
+                                        <button data-modal-hide="default-modal" type="button" class="text-white bg-blue-300 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>
+                                        <button data-modal-hide="default-modal" type="button" class="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
 
                     </div>
                 </div>
@@ -189,7 +223,6 @@ include "../SQL/connect.php";
                         </svg>
                     </div>
                 </div>
-
             </div>
                 <?php  
                 }
@@ -202,14 +235,47 @@ include "../SQL/connect.php";
                         <h2 class="pt-4 text-lg text-font-medium">What is the main objective of this project?</h2>
                     </div>
                     <div class="p-4 flex items-center gap-10 mt-2 text-white">
-                        <a href="" class="flex items-center border border-gray-300 px-2 py-1 rounded-lg">
-                            <svg class="pr-1" xmlns="http://www.w3.org/2000/svg" height="28" width="28" viewBox="0 0 448 512">
+                        <!-- <a href="" class="flex items-center border border-gray-300 px-2 py-1 rounded-lg"> -->
+                        <button data-modal-target="default-modal" data-modal-toggle="default-modal" class="block text-white border border-gray-300 bg-none hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Add Answer +</button>
+                            <!-- <svg class="pr-1" xmlns="http://www.w3.org/2000/svg" height="28" width="28" viewBox="0 0 448 512">
                                 <path fill="#ffffff" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                            </svg>
-                            Add an answer
-                        </a>
+                            </svg> -->
+                            <!-- Add an answer -->
+                        <!-- </a> -->
+                       
                         <p class="border border-gray-300 px-2 rounded-full">HTML</p>
                         <p class="border border-gray-300 px-2 rounded-full">CSS</p>
+
+
+                        <div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div class="relative p-4 w-full max-w-2xl max-h-full">
+                                <!-- Modal content -->
+                                <div class="relative bg-white rounded-lg shadow dark:bg-white-700">
+                                    <!-- Modal header -->
+                                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                        <h3 class="text-xl font-semibold text-gray-900 dark:text-black">
+                                            What is the main objective of this project?
+                                        </h3>
+                                        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                            </svg>
+                                            <span class="sr-only">Close modal</span>
+                                        </button>
+                                    </div>
+                                    <div class="p-4 md:p-5 space-y-4">
+                                        <textarea id="modalContent" class="w-full p-2 border rounded-md text-base leading-relaxed text-black dark:text-gray-400 resize-none" rows="5">
+                                        </textarea>
+                                    </div>
+                                    <!-- Modal footer -->
+                                    <div class="flex items-center justify-end p-4 md:p-5 border-t-0 border-gray-200 rounded-b dark:border-gray-600">
+                                        <button data-modal-hide="default-modal" type="button" class="text-white bg-blue-300 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>
+                                        <button data-modal-hide="default-modal" type="button" class="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
 
                     </div>
                 </div>
@@ -233,7 +299,6 @@ include "../SQL/connect.php";
                         </svg>
                     </div>
                 </div>
-
             </div>
             
 
@@ -241,6 +306,40 @@ include "../SQL/connect.php";
         </div>
 
     </main>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+        const openModalBtn = document.querySelector('[data-modal-toggle="default-modal"]');
+        const closeModalBtn = document.querySelector('[data-modal-hide="default-modal"]');
+        const modal = document.getElementById('default-modal');
+        const modalContent = document.getElementById('modalContent');
+        const addAnswerBtn = document.querySelector('[data-modal-hide="default-modal"]');
+        const cancelBtn = document.querySelector('[data-modal-hide="default-modal"]');
+        const modalTitle = document.getElementById('modalTitle');
+
+        openModalBtn.addEventListener('click', function () {
+            // Set the question in the modal
+            modalTitle.textContent = 'Question:';
+            modalContent.value = openModalBtn.textContent.trim();
+            modal.classList.remove('hidden');
+        });
+
+        closeModalBtn.addEventListener('click', function () {
+            modal.classList.add('hidden');
+        });
+
+        addAnswerBtn.addEventListener('click', function () {
+            // Send textarea data via AJAX if needed
+            // Example without AJAX:
+            alert('Answer added: ' + modalContent.value);
+            modal.classList.add('hidden');
+        });
+
+        cancelBtn.addEventListener('click', function () {
+            modal.classList.add('hidden');
+        });
+    });
+
+    </script>
 </body>
 
 </html>
